deeb2c0bd30fefcc053d915f82eb68efb31cb691
Brendon Ghaderi <3301521+brendong@users.noreply.github.com>
2025-08-08 14:12:40 -0700

    added local playground example

---
+++ examples/00-playground/src/index.ts
@@ -0,0 +1,73 @@
+import { oNetwork, setupGracefulShutdown } from '@olane/o-network';
+import { NodeType, oAddress } from '@olane/o-core';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+(async () => {
+  const network = new oNetwork({
+    // configFilePath: path.join(os.homedir(), '.olane', 'config.json'),
+    nodes: [
+      {
+        type: NodeType.LEADER,
+        address: new oAddress('o://leader'),
+        leader: null,
+        parent: null,
+      },
+      {
+        type: NodeType.NODE,
+        address: new oAddress('o://node'),
+        leader: null,
+        parent: null,
+      },
+    ],
+    plans: [],
+    noIndexNetwork: true,
+  });
+
+  setupGracefulShutdown(
+    async () => {
+      console.log('Stopping o-network...');
+      await network.stop();
+      console.log('o-network stopped successfully');
+    },
+    {
+      timeout: 30000, // 30 seconds timeout
+      onTimeout: () => {
+        console.error('Shutdown timeout reached, forcing exit');
+      },
+    },
+  );
+
+  await network.start();
+  console.log('Network started!!!--------------------------------');
+
+  await network.use(new oAddress('o://vector-store'), {
+    method: 'add_documents',
+    params: {
+      documents: [
+        {
+          pageContent: 'Slack is a messaging tool',
+          metadata: {
+            address: 'o://leader/node/mcp/slack',
+            id: 1,
+          },
+        },
+        {
+          pageContent: 'Dillon sent a message to brendon on slack',
+          metadata: {
+            address: 'o://leader/node/mcp/slack',
+            id: 2,
+          },
+        },
+        {
+          pageContent: "Brendon's slack username is brendon",
+          metadata: {
+            address: 'o://leader/node/mcp/slack',
+            id: 3,
+          },
+        },
+      ],
+    },
+  });
+})();
