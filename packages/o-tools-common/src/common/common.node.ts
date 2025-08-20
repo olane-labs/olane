c8ffd41c65f475e6c3c8785cc2624bed29148412
Brendon Ghaderi <3301521+brendong@users.noreply.github.com>
2025-08-20 15:41:21 -0700

    removed some core dependency confusion

---
+++ packages/o-tools-common/src/common/common.node.ts
@@ -0,0 +1,8 @@
+import { CoreConfig } from '@olane/o-core';
+import { oVirtualTool } from '@olane/o-tool';
+
+export class oCommonNode extends oVirtualTool {
+  constructor(config: CoreConfig) {
+    super(config);
+  }
+}
