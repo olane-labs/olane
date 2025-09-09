a5b627ac4ce35c04d71796bb09d8196340910aa7
Brendon Ghaderi <3301521+brendong@users.noreply.github.com>
2025-09-09 16:39:26 -0700

    began work on re-organizing agents + gateways

--- packages/o-tools-common/src/index.ts
+++ packages/o-tools-common/src/index.ts
@@ -1,6 +1,5 @@
 export * from './registry/index.js';
 export * from './storage/index.js';
-export * from './agent/index.js';
 export * from './init.js';
 export * from './plan/index.js';
 export * from './setup/index.js';
