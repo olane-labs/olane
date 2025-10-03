32daa57272efc49b7e6502ec5893bd73ab294774
Brendon Ghaderi <3301521+brendong@users.noreply.github.com>
2025-10-03 15:01:59 -0700

    major refactor complete

--- packages/o-storage/src/providers/index.ts
+++ packages/o-storage/src/providers/index.ts
@@ -3,3 +3,4 @@ export * from './memory-storage-provider.tool.js';
 export * from './secure-storage-provider.tool.js';
 export * from './storage-provider.tool.js';
 export * from './dht-storage-provider.tool.js';
+export * from './o-lane.storage.js';
