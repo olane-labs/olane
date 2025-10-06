70fe8d988cfea6a498dc209f6a4a4f370fa91d7e
Brendon Ghaderi <3301521+brendong@users.noreply.github.com>
2025-10-06 16:17:22 -0700

    global README added

--- examples/00-playground/jest.config.js
+++
@@ -1,6 +0,0 @@
-// jest.config.js
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node', // Default environment
-  testTimeout: 30000, // Increase test timeout for Puppeteer tests
-};
