1eec33437df4d14da017e851d02e88a352d7d2cf
Brendon Ghaderi <3301521+brendong@users.noreply.github.com>
2025-12-06 15:33:59 -0800

    minor change

--- packages/o-tools-common/src/search/parameters/search.parameters.ts
+++ packages/o-tools-common/src/search/parameters/search.parameters.ts
@@ -1,7 +1,7 @@
 import { oMethod } from '@olane/o-protocol';
 
 export const SEARCH_PARAMS: { [key: string]: oMethod } = {
-  search: {
+  vector: {
     name: 'vector',
     description:
       'Search for data using vector similarity within the Olane OS from a global view.',
